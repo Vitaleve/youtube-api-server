a3aa24e1766d1af7241cfe45dd3b5bff1a76fa4e
Vitalijs Ivanovs <vitalijsivanovs@VitalijsiMac27.fritz.box>
2025-06-20 09:35:56 +0200

    Добавлен запуск Uvicorn с портом для Render

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Query
 from fastapi.responses import JSONResponse
 from yt_dlp import YoutubeDL
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
 app = FastAPI()
 
@@ -29,3 +30,7 @@ async def download_video(url: str = Query(..., description="YouTube URL")):
             }
     except Exception as e:
         return JSONResponse(status_code=500, content={"error": str(e)})
+
+# Запуск сервера вручную
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=10000)
